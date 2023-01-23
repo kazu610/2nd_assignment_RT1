4c46d58568a6fa3674b7cf26f300c9a897665f3f
kazu610 <mutokazuto@gmail.com>
2023-01-23 16:41:58 +0000

    made actionlib part

--- src/node_a.cpp
+++ src/node_a.cpp
@@ -21,30 +21,79 @@ int main(int argc, char **argv){
 	ros::init(argc, argv, "node_a");
 	ros::NodeHandle n;
 
+	//To publish robot_data
 	ros::Subscriber sub = n.subscribe("/odom", 1, data_Callback);
 	ros::Publisher pub = n.advertise<assignment_2_2022::Info>("robot_data",1);
+	
+	ros::ServiceClient client = n.serviceClient<assignment_2_2022::Goal>("/goal_info");
+	assignment_2_2022::Goal goal; //declare
+	
+	double x = 1.0, y = 3.0; //goal position with the initial values 
+	std::string input; //input from terminal
 	actionlib::SimpleActionClient<assignment_2_2022::PlanningAction> ac("/reaching_goal", true);
 	ac.waitForServer(); //waiting for action server start
 	assignment::PlanningGoal target;
-	target.pose.position.x = ;
-	target.pose.position.y = ;
+	target.target_pose.position.x = x;
+	target.target_pose.position.y = y;
 	ac.sendGoal(target);
 	
-ros::Rate loop_rate(10);
-
-int count = 0;
-
-while(ros::ok()){
-	assignment_2_2022::Info data;
-	data.x = x;
-	data.y = y;
-	data.vel_x = vel_x;
-	data.vel_y = vel_y;
+	bool finished_before_timeout = ac.waitForResult(ros::Duration(40.0));
 	
-	pub.publish(data);
-	ros::spinOnce();
-	loop_rate.sleep();
-	++count;
-}
+	ac.waitForResult(); 
+	ros::Rate rate(50);
+
+	while(!finished_before_timeout && ros::ok()){
+		//publish the robot info(position and velocity)
+		assignment_2_2022::Info data;
+		data.x = x;
+		data.y = y;
+		data.vel_x = vel_x;
+		data.vel_y = vel_y;
+		pub.publish(data);
+		
+		//Actionlib part
+		std::cout << "Please type 'g' or 'c'\n'g' setting new goal, 'c' cancelling  the current goal: " << std::endl;
+		std::cin >> input;
+		if (input == "g"){
+			std::cout << "Please enter new goal position (x, y): " << std::endl;
+			std::cin >> x >> y;
+			//send new goal
+			target.target_pose.position.x = x;
+			target.target_pose.position.y = y;
+			ac.snedGoal(target);
+			//update rosparam
+			ros::param::set("des_pos_x", x);
+			ros::param::set("des_pos_y", y);
+		}
+		else if (input == "c"){
+			ac.cancelGoal();
+			ROS_INFO("The goal was cancelled");
+			//service client
+			goal.request.count_r = 0;
+			goal.request.count_c = 1;
+			client.call(goal);
+		}
+		else{
+			std::cout << "Please type valid keys, 'g' or 'c': " << std::endl;
+		}
+		finished_before_timeout = ac.waitForResult(ros::Duration(40.0));
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+	
+	if (finished_before_timeout){
+    		actionlib::SimpleClientGoalState state = ac.getState();
+    		ROS_INFO("The robot has reached the goal: %s",state.toString().c_str());
+    		//service client
+    		goal.request.count_r = 1;
+		goal.request.count_c = 0;
+		client.call(goal);
+  	}
+  	else{
+    		ROS_INFO("Action did not finish before the time out.");
+   		ac.cancelGoal(); 
+   		ROS_INFO("Goal has been cancelled");
+   	}
+	
 return 0;
 }
--- src/node_c.cpp
+++ src/node_c.cpp
@@ -4,13 +4,41 @@
 #include <math.h>
 #include "assignment_2_2022/Info.h"
 
-void data_Callback(const assignment_2_2022::Info& data){
-	float target_x, target_y, dist, ave_vel;
-	ros::param::get("des_pos_x",target_x);
-	ros::param::get("des_pos_y",target_y);
-	dist = sqrd(pow(data->x - target_x, 2.0)+(data->y - target_y, 2.0));
-	ave_vel = sqrd(pow(data->vel_x,2.0)+(data->vel_y, 2.0)); //not sure
-	ROS_INFO("Distance:%f, Velocity:%f", dist, ave_vel);
+float p_x, p_y, v_x, v_y;
+float vel = 0;
+int count = 1;
+
+void data_Callback(const assignment_2_2022::Info::ConstPtr& data){
+	p_x = data->x;
+	p_y = data->y;
+	v_x = data->vel_x;
+	v_y = data->vel_y;
+	robot_status();
+	
+	count += 1;
+}
+
+void robot_status(){
+	ROS_INFO("[Position] x=%f, y=%f", p_x, p_y);
+	ROS_INFO("[Velocity] x=%f, y=%f", v_x, v_y);
+	ROS_INFO("[Distance to goal] %f", distance(p_x, p_y));
+	ROS_INFO("[Average speed] %f", average_speed(v_x, v_y));
+}
+
+float distance(p_x, p_y){
+	ros::param::get("des_pos_x",t_x);
+	ros::param::get("des_pos_y",t_y);
+	dist = sqrd(pow(t_x - p_x, 2.0)+(t_y - p_y, 2.0));
+	
+	return dist;
+}
+
+float average_speed(v_x, v_y){
+	vel += sqrd(pow(v_x,2.0)+(v_y, 2.0));
+	ave_vel = vel/count;
+	
+	return ave_vel;
+	
 }
 
 int main(int argc, char **argv){
@@ -18,8 +46,11 @@ int main(int argc, char **argv){
 	ros::init(argc, argv, "node_c");
 	ros::NodeHandle n;
 
-	ros::subscriber sub = n.subscribe("robot_data", 1, data_Callback);
-
+	ros::subscriber sub = n.subscribe("robot_data", 10, data_Callback);
+	
+	ros::param::get("freq", freq);
+	ros::Rate rate(freq);
+	
 	ros::spin();
 	return 0;
 }
